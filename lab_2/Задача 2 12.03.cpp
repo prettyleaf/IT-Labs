6248d8a32d029f3609c2b106ac5b23a10516f224
itreedwed <itreedwed@gmail.com>
2024-03-25 20:38:46 +0700

    LAB1+LAB2
    Уэээ

---
+++ lab_2/Задача 2 12.03.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int sumOfArray(int* arr, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += *(arr + i);
+    }
+    return sum;
+}
+
+int main() {
+    int array[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(array) / sizeof(array[0]);
+    int totalSum = sumOfArray(array, size);
+    std::cout << "Сумма элементов массива: " << totalSum << endl;
+    return 0;
+}
