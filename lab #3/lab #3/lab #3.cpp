019f571fc8499e3bfacc0370fe75baf0b4f9f91e
doritorainn <106918410+doritorainn@users.noreply.github.com>
2024-03-28 02:19:50 +0700

    Lab #3 template + first task done (might require rewriting)

---
+++ lab #3/lab #3/lab #3.cpp
@@ -0,0 +1,64 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include "lab #3.h"
+
+using namespace std;
+
+void Task01();
+void Task02();
+void Task03();
+// void Task04();
+// void Task05();
+// void Task06();
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	
+	char TaskNumber;
+	cout << "Введите номер задания(1-6)";
+	cin >> TaskNumber;
+	switch (TaskNumber) {
+	case '1': Task01(); break;
+	case '2': Task02(); break;
+	case '3': Task03(); break;
+	// case '4': Task04();
+	// case '5': Task05();
+	// case '6': Task06();
+	}
+	
+	return 0;
+}
+void Task01()
+	{
+		vector<int> R;
+		int amount;
+		cout << "Кол-во сопротивлений? :";
+		cin >> amount;
+		for (int i = 1; i<=amount; i++){
+			cout << "Введите " << i << "-ое сопротивление: ";
+			cin >> R.emplace_back();
+		}
+		int R_;
+		for (int i = 0; i<amount; i++){
+			R_ = R_ + R[i];
+		}
+		cout << (R_ / amount) << endl;
+	}
+void Task02()
+	{
+		int a = 2;
+		cout << a << endl;
+	}
+void Task03()
+	{
+		int a = 3;
+		cout << a << endl;
+	}
