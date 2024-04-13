75f5e42a67c0cd21188cee61abac09a140f4406e
bisvi bisvi <81024464+Rockstar234@users.noreply.github.com>
2024-04-13 11:55:09 +0300

    lab 2 tasks 1-4 done

--- lab #3/lab #3/lab #3.cpp
+++ lab #3/lab #3/lab #3.cpp
@@ -12,14 +12,14 @@ void Task07();
 void Task08();
 void Task09();
 void Task10();
-void Task11();
-void Task12();
+//void Task11();
+//void Task12();
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
-	
+
 	char TaskNumber;
 	cout << "Введите номер задания(1-12): ";
 	cin >> TaskNumber;
@@ -34,105 +34,105 @@ int main()
 	case '8': Task08(); break;
 	case '9': Task09(); break;
 	case '10': Task10(); break;
-	case '11': Task11(); break;
-	case '12': Task12(); break;
+		//case '11': Task11(); break;
+		//case '12': Task12(); break;
 	}
-	
+
 	return 0;
 }
 void Task01()
-	{
-		vector<int> R;
-		int amount;
-		cout << "Кол-во сопротивлений? : ";
-		cin >> amount;
-		for (int i = 1; i<=amount; i++){
-			cout << "Введите " << i << "-ое сопротивление: ";
-			cin >> R.emplace_back();
-		}
-		int R_ = 0;
-		for (int i = 0; i<amount; i++){
-			R_ = R_ + R[i];
-		}
-		cout << (R_ / amount) << endl;
+{
+	vector<int> R;
+	int amount;
+	cout << "Кол-во сопротивлений? : ";
+	cin >> amount;
+	for (int i = 1; i <= amount; i++) {
+		cout << "Введите " << i << "-ое сопротивление: ";
+		cin >> R.emplace_back();
 	}
+	int R_ = 0;
+	for (int i = 0; i < amount; i++) {
+		R_ = R_ + R[i];
+	}
+	cout << (R_ / amount) << endl;
+}
 void Task02()
-	{
-		int a, square, volume;
-		cout << "Длина ребра куба? : ";
-		cin >> a;
-		square = pow(a, 2);
-		volume = pow(a, 3);
-		cout << "Площадь: " << square << ", Объём:  " << volume << endl;
-	}
+{
+	int a, square, volume;
+	cout << "Длина ребра куба? : ";
+	cin >> a;
+	square = pow(a, 2);
+	volume = pow(a, 3);
+	cout << "Площадь: " << square << ", Объём:  " << volume << endl;
+}
 void Task03()
-	{
-		vector<int> a;
-		int p = 0;
-		int S = 0;
-		for (int i = 1; i<=3; i++){
-			cout << "Введите " << i << "-ую сторону: ";
-			cin >> a.emplace_back();
-		}
-		p = (a[0]+a[1]+a[2])/2;
-		S = sqrt(p*(p-a[0])*(p-a[1])*(p-a[2]));
-		cout << "Площадь по формуле Герона: " << S << endl;
+{
+	vector<int> a;
+	int p = 0;
+	int S = 0;
+	for (int i = 1; i <= 3; i++) {
+		cout << "Введите " << i << "-ую сторону: ";
+		cin >> a.emplace_back();
 	}
+	p = (a[0] + a[1] + a[2]) / 2;
+	S = sqrt(p * (p - a[0]) * (p - a[1]) * (p - a[2]));
+	cout << "Площадь по формуле Герона: " << S << endl;
+}
 void Task04()
-	{
+{
 	for (int num = 0; num < 3; num++) {
-			// 10 случайных чисел в диапазоне от 0 до 20000 и делим их на 10000, чтобы получить значения в интервале
-			double result = (1.0 + (rand() % 100001)) / 10000;
-			cout << result << "- Взятое число\n";
-			if (result > 1 && result < 3) {
-				cout << "Число - " << result << " - included!\n";
-			}
-			else cout << "Число - " << result << " - not included!\n";
+		// 10 случайных чисел в диапазоне от 0 до 20000 и делим их на 10000, чтобы получить значения в интервале
+		double result = (1.0 + (rand() % 100001)) / 10000;
+		cout << result << "- Взятое число\n";
+		if (result > 1 && result < 3) {
+			cout << "Число - " << result << " - included!\n";
 		}
+		else cout << "Число - " << result << " - not included!\n";
+	}
 
-	}
+}
 void Task05()
-	{
-		double x, y;
-		cout << "Введите X: \n";
-		cin >> x;
-		cout << "Введите Y: \n";
-		cin >> y;
-		if (x == y) {
-			cout << "Используйте не одинаковые числа!\n";
-			exit(0);
-		}
-		double min, max;
-		if (x < y) {
-			min = x;
-			max = y;
-		}
-		else {
-			min = y;
-			max = x;
-		}
-		min = (min + max) / 2;
-		max = 2 * min * max;
-		cout << "X = " << min << endl;
-		cout << "Y = " << max << endl;
+{
+	double x, y;
+	cout << "Введите X: \n";
+	cin >> x;
+	cout << "Введите Y: \n";
+	cin >> y;
+	if (x == y) {
+		cout << "Используйте не одинаковые числа!\n";
+		exit(0);
 	}
+	double min, max;
+	if (x < y) {
+		min = x;
+		max = y;
+	}
+	else {
+		min = y;
+		max = x;
+	}
+	min = (min + max) / 2;
+	max = 2 * min * max;
+	cout << "X = " << min << endl;
+	cout << "Y = " << max << endl;
+}
 void Task06()
-	{
-		double x, y, z;
-		cout << "Введите длины сторон треугольника (x, y, z): \n";
-		cin >> x >> y >> z;
-		// правило: большая сторона меньше суммы двух других сторон
-		bool triangle_rule = false;
-		if (x + y > z && x + z > y && y + z > x) {
-			triangle_rule = true;
-		}
-		if (triangle_rule) {
-			cout << "СУЩЕСТВУЕТ!\n" << endl;
-		}
-		else {
-			cout << "Такого треугольника существовать не может.\n" << endl;
-		}
+{
+	double x, y, z;
+	cout << "Введите длины сторон треугольника (x, y, z): \n";
+	cin >> x >> y >> z;
+	// правило: большая сторона меньше суммы двух других сторон
+	bool triangle_rule = false;
+	if (x + y > z && x + z > y && y + z > x) {
+		triangle_rule = true;
 	}
+	if (triangle_rule) {
+		cout << "СУЩЕСТВУЕТ!\n" << endl;
+	}
+	else {
+		cout << "Такого треугольника существовать не может.\n" << endl;
+	}
+}
 void Task07()
 {
 	int a, b, n;
@@ -156,12 +156,56 @@ void Task07()
 void Task08()
 {
 	int n;
-	cout << "Введите число N: ";
+	cout << "Введите число N: \n";
 	cin >> n;
 
 	int hours = n / 3600;
 	int minutes = (n % 3600) / 60;
 	int seconds = (n % 3600) % 60;
+	// приписывается 0 если итоговое число меньше 10, т.е. не двузначное чтобы всё было по условию задачи
+	cout << hours << ":" << (minutes < 10 ? "0" : "") << minutes << ":" << (seconds < 10 ? "0" : "") << seconds << endl;
+}
+void Task09()
+{
+	int velocity, time;
+	cout << "Введите скорость V: \n";
+	cin >> velocity;
+	// rule check for entered velocity
+	if (velocity < -1000 || velocity > 1000) {
+		cout << "Пожалуйста, придерживайтесь условия задачи: -1000 <= V <= 1000 !!!\n";
+		exit(0);
+	}
+	cout << "Введите время T: \n";
+	cin >> time;
+	// rule check for entered time
+	if (time > 1000 || time < 0) {
+		cout << "Пожалуйста, придерживайтесь условия задачи: 0 <= T <= 1000 !!!\n";
+		exit(0);
+	}
+	int distance = velocity * time;
+	int result = distance % 109;
+	if (velocity < 0) {
+		result = (109 - result) % 109;
+	}
+	cout << "Вася остановился на отметке: " << result << endl;
+}
+void Task10() {
+	int year;
+	cout << "Введите год: ";
+	cin >> year;
 
-	// to be continued 
+	bool LeapYear;
+	if (year % 4 == 0 && year % 100 != 0) {
+		LeapYear = true;
+	}
+	else if (year % 400 == 0) {
+		LeapYear = false;
+	}
+
+	if (LeapYear) {
+		cout << year << " год является високосным. В этом году 366 дней.\n" << endl;
+	}
+	else {
+		cout << year << " год НЕ является високосным. В этом году 365 дней.\n" << endl;
+	}
 }
