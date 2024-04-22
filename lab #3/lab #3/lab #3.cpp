aaa24675460bea580ebc5c3c96d121a4a6d015f4
Axiide13 <164883249+Axiide13@users.noreply.github.com>
2024-04-22 16:35:58 +0700

    still requires fix

--- lab #3/lab #3/lab #3.cpp
+++ lab #3/lab #3/lab #3.cpp
@@ -372,14 +372,16 @@ void Task18() {
 	vector<int> array/*(size)*/;
 	while (ss >> num)
 		array.push_back(num);
-	for (int i = 0; i < size - 1; i++)
-		for (int j = 0; j < size - 1; j++)
-			if (array[j] > 0)
-				if (array[j + 1] < 0)
-				{
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (array[j] > 0) {
+				if (array[j + 1] < 0) {
 					int temp;
 					temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 				}
+			}
+		}
+	}
 }
